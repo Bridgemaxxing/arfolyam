8b9299038fe6fe3ab09d80ccfa770bc0ce36eeeb
Bridgemaxxing <marthajozsef2@gmail.com>
2025-05-19 12:56:55 +0200

    elso_resz_kesz

---
+++ alap.js
@@ -0,0 +1,44 @@
+fetch("https://api.exchangerate-api.com/v4/latest/usd")
+.then(x =>x.json())
+.then(y => megjelenitUSD(y))
+
+function megjelenitUSD(y){
+    console.log(y)
+    document.getElementById("usd").innerHTML=y.rates.HUF+" FT"
+}
+
+fetch("https://api.exchangerate-api.com/v4/latest/eur")
+.then(x =>x.json())
+.then(y => megjelenitEUR(y))
+
+function megjelenitEUR(y){
+    console.log(y)
+    document.getElementById("eur").innerHTML=y.rates.HUF+" FT"
+}
+
+fetch("https://api.exchangerate-api.com/v4/latest/gbp")
+.then(x =>x.json())
+.then(y => megjelenitGBP(y))
+
+function megjelenitGBP(y){
+    console.log(y)
+    document.getElementById("gbp").innerHTML=y.rates.HUF+" FT"
+}
+
+fetch("https://api.exchangerate-api.com/v4/latest/ron")
+.then(x =>x.json())
+.then(y => megjelenitEUR(y))
+
+function megjelenitEUR(y){
+    console.log(y)
+    document.getElementById("lej").innerHTML=y.rates.HUF+" FT"
+}
+
+fetch("https://api.exchangerate-api.com/v4/latest/cny")
+.then(x =>x.json())
+.then(y => megjelenitCNY(y))
+
+function megjelenitCNY(y){
+    console.log(y)
+    document.getElementById("juan").innerHTML=y.rates.HUF+" FT"
+}
